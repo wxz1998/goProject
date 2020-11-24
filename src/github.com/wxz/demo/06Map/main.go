1503493d1cea5e36589e9a14254cd852fb0a53af
wxz1998 <wxz18737676272@163.com>
2020-11-24 23:02:09 +0800

    20201124

--- src/github.com/wxz/demo/06Map/main.go
+++ src/github.com/wxz/demo/06Map/main.go
@@ -1,15 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	// a := [5]int{1, 2, 3, 4, 5}
 	// s := a[1:3] // s := a[low:high]
 	// fmt.Printf("a:%v s:%v len(s):%v cap(s):%v\n", a, s, len(s), cap(s))
 
-	// a := [...]float64{67.7, 89.8, 21, 78}
-	// sum := float64(0)
-	// for i, v := range a { //range returns both the index and value
-	// 	fmt.Printf("%d the element of a is %.2f\n", i, v)
-	// 	sum += v
-	// }
-	// fmt.Println("\nsum of all elements of a", sum)
+	a := [...]float64{67.7, 89.8, 21, 78}
+	sum := float64(0)
+	for i, v := range a { //range returns both the index and value
+		fmt.Printf("%d the element of a is %.2f\n", i, v)
+		sum += v
+	}
+	fmt.Println("\nsum of all elements of a", sum)
 }
