d7a63ba20b69af8d7c0825042fb0848eb8319fc3
wxz1998 <wxz18737676272@163.com>
2020-11-26 17:07:05 +0800

    20201126

--- src/github.com/wxz/demo/07Map/main.go
+++ src/github.com/wxz/demo/07Map/main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sort"
-	"time"
 )
 
 func main() {
@@ -82,24 +79,39 @@ func main() {
 	// }
 
 	// 按照指定顺序遍历map
-	rand.Seed(time.Now().UnixNano()) //初始化随机数种子
+	// rand.Seed(time.Now().UnixNano()) //初始化随机数种子
 
-	var scoreMap = make(map[string]int, 200)
+	// var scoreMap = make(map[string]int, 200)
 
-	for i := 0; i < 100; i++ {
-		key := fmt.Sprintf("stu%02d", i) //生成stu开头的字符串
-		value := rand.Intn(100)          //生成0~99的随机整数
-		scoreMap[key] = value
+	// for i := 0; i < 100; i++ {
+	// 	key := fmt.Sprintf("stu%02d", i) //生成stu开头的字符串
+	// 	value := rand.Intn(100)          //生成0~99的随机整数
+	// 	scoreMap[key] = value
+	// }
+	// //取出map中的所有key存入切片keys
+	// var keys = make([]string, 0, 200)
+	// for key := range scoreMap {
+	// 	keys = append(keys, key)
+	// }
+	// //对切片进行排序
+	// sort.Strings(keys)
+	// //按照排序后的key遍历map
+	// for _, key := range keys {
+	// 	fmt.Println(key, scoreMap[key])
+	// }
+
+	// 元素为map类型的切片
+	var mapSlice = make([]map[string]string, 3)
+	for index, value := range mapSlice {
+		fmt.Printf("index:%d value:%v\n", index, value)
 	}
-	//取出map中的所有key存入切片keys
-	var keys = make([]string, 0, 200)
-	for key := range scoreMap {
-		keys = append(keys, key)
-	}
-	//对切片进行排序
-	sort.Strings(keys)
-	//按照排序后的key遍历map
-	for _, key := range keys {
-		fmt.Println(key, scoreMap[key])
+	fmt.Println("after init")
+	// 对切片中的map元素进行初始化
+	mapSlice[0] = make(map[string]string, 10)
+	mapSlice[0]["name"] = "小王子"
+	mapSlice[0]["password"] = "123456"
+	mapSlice[0]["address"] = "沙河"
+	for index, value := range mapSlice {
+		fmt.Printf("index:%d value:%v\n", index, value)
 	}
 }
