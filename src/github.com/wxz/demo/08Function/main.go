5ccb71c9af41ffa699f414989f0d8a8fba2a86be
wxz1998 <wxz18737676272@163.com>
2020-11-26 23:01:18 +0800

    20201126

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -180,3 +180,14 @@ package main
 
 // 函数作为返回值
 // 函数也可以作为返回值：
+// func do(s string) (func(int, int) int, error) {
+// 	switch s {
+// 	case "+":
+// 		return add, nil
+// 	case "-":
+// 		return sub, nil
+// 	default:
+// 		err := errors.New("无法识别的操作符")
+// 		return nil, err
+// 	}
+// }
