0ce92d9724e574adea2a3a6550ab0a426bfafeb3
wxz1998 <wxz18737676272@163.com>
2020-11-26 21:20:22 +0800

    20201126

--- src/github.com/wxz/demo/07Map/main.go
+++ src/github.com/wxz/demo/07Map/main.go
@@ -101,32 +101,37 @@ func main() {
 	// }
 
 	// 元素为map类型的切片
-	// var mapSlice = make([]map[string]string, 3)
-	// for index, value := range mapSlice {
-	// 	fmt.Printf("index:%d value:%v\n", index, value)
-	// }
-	// fmt.Println("after init")
-	// // 对切片中的map元素进行初始化
-	// mapSlice[0] = make(map[string]string, 10)
-	// mapSlice[0]["name"] = "小王子"
-	// mapSlice[0]["password"] = "123456"
-	// mapSlice[0]["address"] = "沙河"
-	// for index, value := range mapSlice {
-	// 	fmt.Printf("index:%d value:%v\n", index, value)
-	// }
+	var mapSlice = make([]map[string]string, 3)
+	for index, value := range mapSlice {
+		fmt.Printf("index:%d value:%v\n", index, value)
+	}
+	fmt.Println("after init")
+	// 对切片中的map元素进行初始化
+	mapSlice[0] = make(map[string]string, 10)
+	mapSlice[0]["name"] = "小王子"
+	mapSlice[0]["password"] = "123456"
+	mapSlice[0]["address"] = "沙河"
+	mapSlice[1] = make(map[string]string)
+	mapSlice[1]["name"] = "wxz"
+	mapSlice[1]["sexy"] = "man"
+	mapSlice[2] = make(map[string]string)
+	mapSlice[2]["name"] = "123"
+	for index, value := range mapSlice {
+		fmt.Printf("index:%d value:%v\n", index, value)
+	}
 
 	// 值为切片类型的map
-	var sliceMap = make(map[string][]string, 3)
-	fmt.Println(sliceMap)
-	fmt.Println("after init")
-	key := "中国"
-	value, ok := sliceMap[key]
-	if !ok {
-		value = make([]string, 0, 2)
-	}
-	value = append(value, "北京", "上海")
-	sliceMap[key] = value
-	fmt.Println(sliceMap)
+	// var sliceMap = make(map[string][]string, 3)
+	// fmt.Println(sliceMap)
+	// fmt.Println("after init")
+	// key := "中国"
+	// value, ok := sliceMap[key]
+	// if !ok {
+	// 	value = make([]string, 0, 2)
+	// }
+	// value = append(value, "北京", "上海")
+	// sliceMap[key] = value
+	// fmt.Println(sliceMap)
 
 	// output:
 	// map[]
