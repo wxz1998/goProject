ec1b2feb755cc1c222b77490bcb8cf0facdd09a2
wxz1998 <wxz18737676272@163.com>
2020-11-26 22:27:27 +0800

    20201126

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -1,53 +1,51 @@
 package main
 
-import "fmt"
-
 func main() {
-	sayHello()
-	ret := intSum(10, 20)
-	fmt.Println(ret)
+	// sayHello()
+	// ret := intSum(10, 20)
+	// fmt.Println(ret)
 
-	ret1 := intSum2()
-	ret2 := intSum2(10)
-	ret3 := intSum2(10, 20)
-	ret4 := intSum2(10, 20, 30)
-	fmt.Println(ret1, ret2, ret3, ret4) //0 10 30 60
+	// ret1 := intSum2()
+	// ret2 := intSum2(10)
+	// ret3 := intSum2(10, 20)
+	// ret4 := intSum2(10, 20, 30)
+	// fmt.Println(ret1, ret2, ret3, ret4) //0 10 30 60
 
-	ret5 := intSum3(100)
-	ret6 := intSum3(100, 10)
-	ret7 := intSum3(100, 10, 20)
-	ret8 := intSum3(100, 10, 20, 30)
-	fmt.Println(ret5, ret6, ret7, ret8) //100 110 130 160
+	// ret5 := intSum3(100)
+	// ret6 := intSum3(100, 10)
+	// ret7 := intSum3(100, 10, 20)
+	// ret8 := intSum3(100, 10, 20, 30)
+	// fmt.Println(ret5, ret6, ret7, ret8) //100 110 130 160
 }
 
-func sayHello() {
-	fmt.Println("hello")
-}
+// func sayHello() {
+// 	fmt.Println("hello")
+// }
 
-func intSum(x int, y int) int { // 可以写成"x , y int"类型简写
-	return x + y
-}
+// func intSum(x int, y int) int { // 可以写成"x , y int"类型简写
+// 	return x + y
+// }
 
 // 可变参数是指函数的参数数量不固定。Go语言中的可变参数通过在参数名后加...来标识。
 // 注意：可变参数通常要作为函数的最后一个参数。
-func intSum2(x ...int) int {
-	fmt.Println(x) //x是一个切片
-	sum := 0
-	for _, v := range x {
-		sum = sum + v
-	}
-	return sum
-}
+// func intSum2(x ...int) int {
+// 	fmt.Println(x) //x是一个切片
+// 	sum := 0
+// 	for _, v := range x {
+// 		sum = sum + v
+// 	}
+// 	return sum
+// }
 
 // 固定参数搭配可变参数使用时，可变参数要放在固定参数的后面
-func intSum3(x int, y ...int) int {
-	fmt.Println(x, y)
-	sum := x
-	for _, v := range y {
-		sum = sum + v
-	}
-	return sum
-}
+// func intSum3(x int, y ...int) int {
+// 	fmt.Println(x, y)
+// 	sum := x
+// 	for _, v := range y {
+// 		sum = sum + v
+// 	}
+// 	return sum
+// }
 
 // 本质上，函数的可变参数是通过切片来实现的。
 
@@ -72,3 +70,6 @@ func intSum3(x int, y ...int) int {
 // 	}
 // 	...
 // }
+
+// 函数进阶
+// 变量作用域
