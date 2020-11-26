ce7c5352fde9dc586e4450853408d05b59b3096f
wxz1998 <wxz18737676272@163.com>
2020-11-26 22:09:45 +0800

    20201126

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -6,6 +6,11 @@ func main() {
 	sayHello()
 	ret := intSum(10, 20)
 	fmt.Println(ret)
+	ret1 := intSum2()
+	ret2 := intSum2(10)
+	ret3 := intSum2(10, 20)
+	ret4 := intSum2(10, 20, 30)
+	fmt.Println(ret1, ret2, ret3, ret4) //0 10 30 60
 }
 
 func sayHello() {
@@ -14,4 +19,13 @@ func sayHello() {
 
 func intSum(x int, y int) int { // 可以写成"x , y int"类型简写
 	return x + y
+}
+
+func intSum2(x ...int) int {
+	fmt.Println(x) //x是一个切片
+	sum := 0
+	for _, v := range x {
+		sum = sum + v
+	}
+	return sum
 }
