6f6ae83bdef676f7e14dada74f1c4ddc58de4c49
wxz1998 <wxz18737676272@163.com>
2020-11-26 15:40:59 +0800

    20201126

--- src/github.com/wxz/demo/06Map/main.go
+++ src/github.com/wxz/demo/06Map/main.go
@@ -92,9 +92,39 @@ func main() {
 	// 	用 make 创建切片
 	// 内置函数 func make([]T, len, cap) []T 可以用来创建切片，该函数接受长度和容量作为参数，返回切片。
 	// 容量是可选的，默认与长度相同。使用 make 函数将会创建一个数组并返回它的切片。
-	i := make([]int, 5, 5)
-	fmt.Println(i)
+	// i := make([]int, 5, 5)
+	// fmt.Println(i)
 	// 用 make 创建的切片的元素值默认为 0 值。上面的程序输出为：[0 0 0 0 0]。
+
+	// 追加元素到切片
+	// 数组是固定长度的，它们的长度不能动态增加。而切片是动态的，可以使用内置函数 append 添加元素到切片。
+	// append 的函数原型为：append(s []T, x ...T) []T。
+	// x …T 表示 append 函数可以接受的参数个数是可变的。这种函数叫做变参函数。
+
+	// 问题：如果切片是建立在数组之上的，而数组本身不能改变长度，那么切片是如何动态改变长度的呢？
+	// 实际发生的情况是，当新元素通过调用 append 函数追加到切片末尾时，如果超出了容量，append 内部会创建一个新的数组。
+	// 并将原有数组的元素被拷贝给这个新的数组，最后返回建立在这个新数组上的切片。
+	// 这个新切片的容量是旧切片的二倍（译者注：当超出切片的容量时，append 将会在其内部创建新的数组，该数组的大小是原切片容量的 2 倍。
+	// 最后 append 返回这个数组的全切片，即从 0 到 length - 1 的切片）。
+	// cars := []string{"Ferrari", "Honda", "Ford"}
+	// fmt.Println("cars:", cars, "has old length", len(cars), "and capacity", cap(cars)) //capacity of cars is 3
+	// cars = append(cars, "Toyota")
+	// fmt.Println("cars:", cars, "has new length", len(cars), "and capacity", cap(cars)) //capacity of cars is doubled to 6
+
+	// 切片的 0 值为 nil。一个 nil 切片的长度和容量都为 0。可以利用 append 函数给一个 nil 切片追加值。
+
+	// var names []string //zero value of a slice is nil
+	// if names == nil {
+	// 	fmt.Println("slice is nil going to append")
+	// 	names = append(names, "John", "Sebastian", "Vinay")
+	// 	fmt.Println("names contents:", names)
+	// }
+
+	// 可以使用 ... 操作符将一个切片追加到另一个切片末尾：
+	veggies := []string{"potatoes", "tomatoes", "brinjal"}
+	fruits := []string{"oranges", "apples"}
+	food := append(veggies, fruits...)
+	fmt.Println("food:", food)
 }
 
 // 使用2个range,嵌套的方式打印多维数组
