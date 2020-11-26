59b4cc8780c4d104b222918b3cbd477830103be8
wxz1998 <wxz18737676272@163.com>
2020-11-26 22:12:50 +0800

    20201126

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -6,11 +6,18 @@ func main() {
 	sayHello()
 	ret := intSum(10, 20)
 	fmt.Println(ret)
+
 	ret1 := intSum2()
 	ret2 := intSum2(10)
 	ret3 := intSum2(10, 20)
 	ret4 := intSum2(10, 20, 30)
 	fmt.Println(ret1, ret2, ret3, ret4) //0 10 30 60
+
+	ret5 := intSum3(100)
+	ret6 := intSum3(100, 10)
+	ret7 := intSum3(100, 10, 20)
+	ret8 := intSum3(100, 10, 20, 30)
+	fmt.Println(ret5, ret6, ret7, ret8) //100 110 130 160
 }
 
 func sayHello() {
@@ -27,6 +34,16 @@ func intSum2(x ...int) int {
 	fmt.Println(x) //x是一个切片
 	sum := 0
 	for _, v := range x {
+		sum = sum + v
+	}
+	return sum
+}
+
+// 固定参数搭配可变参数使用时，可变参数要放在固定参数的后面
+func intSum3(x int, y ...int) int {
+	fmt.Println(x, y)
+	sum := x
+	for _, v := range y {
 		sum = sum + v
 	}
 	return sum
