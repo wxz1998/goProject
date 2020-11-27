b982930271655cc3f8b7e94e7ec4a53b5ce32ae6
wxz1998 <wxz18737676272@163.com>
2020-11-27 22:58:47 +0800

    20201127

--- src/github.com/wxz/demo/09Pointer/main.go
+++ src/github.com/wxz/demo/09Pointer/main.go
@@ -1,4 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
+	a := 10
+	b := &a                            // 把a的内存地址返回给b
+	fmt.Printf("a:%d ptr:%p\n", a, &a) // a的值和a的内存地址 a:10 ptr:0xc0000160a0
+	fmt.Printf("b:%p type:%T\n", b, b) // b的值和类型 b:0xc0000160a0 type:*int
+	fmt.Println(&b)                    // 变量b的内存地址 0xc000006028
 }
