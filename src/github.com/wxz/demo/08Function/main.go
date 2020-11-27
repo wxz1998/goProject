47b6de53520f4d1c6d7986d9fe5f041a8c91adc6
wxz1998 <wxz18737676272@163.com>
2020-11-27 17:59:31 +0800

    20201127

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -276,3 +276,26 @@ func main() {
 // 	fmt.Println(jpgFunc("test")) //test.jpg
 // 	fmt.Println(txtFunc("test")) //test.txt
 // }
+
+// 闭包进阶示例3：
+
+// func calc(base int) (func(int) int, func(int) int) {
+// 	add := func(i int) int {
+// 		base += i
+// 		return base
+// 	}
+
+// 	sub := func(i int) int {
+// 		base -= i
+// 		return base
+// 	}
+// 	return add, sub
+// }
+
+// func main() {
+// 	f1, f2 := calc(10)
+// 	fmt.Println(f1(1), f2(2)) //11 9
+// 	fmt.Println(f1(3), f2(4)) //12 8
+// 	fmt.Println(f1(5), f2(6)) //13 7
+// }
+// 闭包其实并不复杂，只要牢记闭包=函数+引用环境。
