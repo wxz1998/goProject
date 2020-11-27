6a66d6a1fb20910a09fa3a56a57df31268a25f12
wxz1998 <wxz18737676272@163.com>
2020-11-27 23:02:25 +0800

    20201127

--- src/github.com/wxz/demo/09Pointer/main.go
+++ src/github.com/wxz/demo/09Pointer/main.go
@@ -17,9 +17,9 @@ import "fmt"
 func main() {
 	//指针取值
 	a := 10
-	b := &a // 取变量a的地址，将指针保存到b中
-	fmt.Printf("type of b:%T\n", b)
-	c := *b // 指针取值（根据指针去内存取值）
-	fmt.Printf("type of c:%T\n", c)
-	fmt.Printf("value of c:%v\n", c)
+	b := &a                          // 取变量a的地址，将指针保存到b中
+	fmt.Printf("type of b:%T\n", b)  // type of b:*int
+	c := *b                          // 指针取值（根据指针去内存取值）
+	fmt.Printf("type of c:%T\n", c)  // type of c:int
+	fmt.Printf("value of c:%v\n", c) // value of c:10
 }
