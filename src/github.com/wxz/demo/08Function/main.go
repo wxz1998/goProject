2ad0f675e98b79c402bb0a892c3bb5e1fa8b41cd
wxz1998 <wxz18737676272@163.com>
2020-11-27 18:10:50 +0800

    20201127

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -391,3 +391,31 @@ func main() {
 
 // 程序运行期间funcB中引发了panic导致程序崩溃，异常退出了。
 // 这个时候我们就可以通过recover将程序恢复回来，继续往后执行。
+// func funcA() {
+// 	fmt.Println("func A")
+// }
+
+// func funcB() {
+// 	defer func() {
+// 		err := recover()
+// 		//如果程序出出现了panic错误,可以通过recover恢复过来
+// 		if err != nil {
+// 			fmt.Println("recover in B")
+// 		}
+// 	}()
+// 	panic("panic in B")
+// }
+
+// func funcC() {
+// 	fmt.Println("func C")
+// }
+// func main() {
+// 	funcA()
+// 	funcB()
+// 	funcC()
+// }
+
+// 注意：
+
+// recover()必须搭配defer使用。
+// defer一定要在可能引发panic的语句之前定义。
