4bcabe0abaeb513218ee6ab374159739c1729d22
wxz1998 <wxz18737676272@163.com>
2020-11-27 18:00:48 +0800

    20201127

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -304,3 +304,11 @@ func main() {
 // Go语言中的defer语句会将其后面跟随的语句进行延迟处理。
 // 在defer归属的函数即将返回时，将延迟处理的语句按defer定义的逆序进行执行，
 // 也就是说，先被defer的语句最后被执行，最后被defer的语句，最先被执行。
+
+// func main() {
+// 	fmt.Println("start")
+// 	defer fmt.Println(1)
+// 	defer fmt.Println(2)
+// 	defer fmt.Println(3)
+// 	fmt.Println("end")
+// }
