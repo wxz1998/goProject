c763e3bd4dbc5b5ab3033196a531c322d91471af
wxz1998 <wxz18737676272@163.com>
2020-11-27 23:01:29 +0800

    20201127

--- src/github.com/wxz/demo/09Pointer/main.go
+++ src/github.com/wxz/demo/09Pointer/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // 指针地址和指针类型
 // Go语言中的指针不能进行偏移和运算，因此Go语言中的指针操作非常简单，我们只需要记住两个符号：&（取地址）和*（根据地址取值）
 // func main() {
@@ -12,3 +14,12 @@ package main
 
 // 指针取值
 // 在对普通变量使用&操作符取地址后会获得这个变量的指针，然后可以对指针使用*操作，也就是指针取值
+func main() {
+	//指针取值
+	a := 10
+	b := &a // 取变量a的地址，将指针保存到b中
+	fmt.Printf("type of b:%T\n", b)
+	c := *b // 指针取值（根据指针去内存取值）
+	fmt.Printf("type of c:%T\n", c)
+	fmt.Printf("value of c:%v\n", c)
+}
