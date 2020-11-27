3ab78bb2eb53eefe2ca8572bd93ee5c0a9547975
wxz1998 <wxz18737676272@163.com>
2020-11-27 18:08:28 +0800

    20201127

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -351,3 +351,20 @@ func main() {
 // 	fmt.Println(f3())
 // 	fmt.Println(f4())
 // }
+
+// defer面试题
+// 提示：defer注册要延迟执行的函数时该函数所有的参数都需要确定其值
+// func calc(index string, a, b int) int {
+// 	ret := a + b
+// 	fmt.Println(index, a, b, ret)
+// 	return ret
+// }
+
+// func main() {
+// 	x := 1
+// 	y := 2
+// 	defer calc("AA", x, calc("A", x, y))
+// 	x = 10
+// 	defer calc("BB", x, calc("B", x, y))
+// 	y = 20
+// }
