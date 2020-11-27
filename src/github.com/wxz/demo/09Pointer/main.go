5b9590c61a11531bb6d50fe54871ade6a3869c3d
wxz1998 <wxz18737676272@163.com>
2020-11-27 23:04:06 +0800

    20201127

--- src/github.com/wxz/demo/09Pointer/main.go
+++ src/github.com/wxz/demo/09Pointer/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // 指针地址和指针类型
 // Go语言中的指针不能进行偏移和运算，因此Go语言中的指针操作非常简单，我们只需要记住两个符号：&（取地址）和*（根据地址取值）
 // func main() {
@@ -14,15 +12,15 @@ import "fmt"
 
 // 指针取值
 // 在对普通变量使用&操作符取地址后会获得这个变量的指针，然后可以对指针使用*操作，也就是指针取值
-func main() {
-	//指针取值
-	a := 10
-	b := &a                          // 取变量a的地址，将指针保存到b中
-	fmt.Printf("type of b:%T\n", b)  // type of b:*int
-	c := *b                          // 指针取值（根据指针去内存取值）
-	fmt.Printf("type of c:%T\n", c)  // type of c:int
-	fmt.Printf("value of c:%v\n", c) // value of c:10
-}
+// func main() {
+// 	//指针取值
+// 	a := 10
+// 	b := &a                          // 取变量a的地址，将指针保存到b中
+// 	fmt.Printf("type of b:%T\n", b)  // type of b:*int
+// 	c := *b                          // 指针取值（根据指针去内存取值）
+// 	fmt.Printf("type of c:%T\n", c)  // type of c:int
+// 	fmt.Printf("value of c:%v\n", c) // value of c:10
+// }
 
 // 总结： 取地址操作符&和取值操作符*是一对互补操作符，&取出地址，*根据地址取出地址指向的值。
 
