acc12a0b76ddbcb8552c4f681e683747ab0823bd
wxz1998 <wxz18737676272@163.com>
2020-11-27 17:58:47 +0800

    20201127

--- src/github.com/wxz/demo/08Function/main.go
+++ src/github.com/wxz/demo/08Function/main.go
@@ -258,3 +258,21 @@ func main() {
 	fmt.Println(f1(40)) //60
 	fmt.Println(f1(50)) //110
 }
+
+// 闭包进阶示例2：
+
+// func makeSuffixFunc(suffix string) func(string) string {
+// 	return func(name string) string {
+// 		if !strings.HasSuffix(name, suffix) {
+// 			return name + suffix
+// 		}
+// 		return name
+// 	}
+// }
+
+// func main() {
+// 	jpgFunc := makeSuffixFunc(".jpg")
+// 	txtFunc := makeSuffixFunc(".txt")
+// 	fmt.Println(jpgFunc("test")) //test.jpg
+// 	fmt.Println(txtFunc("test")) //test.txt
+// }
