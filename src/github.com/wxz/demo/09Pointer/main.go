9050b4abcebd702a0a08c93482d5fe7c39dd5d8b
wxz1998 <wxz18737676272@163.com>
2020-11-28 11:52:11 +0800

    20201128

--- src/github.com/wxz/demo/09Pointer/main.go
+++ src/github.com/wxz/demo/09Pointer/main.go
@@ -105,3 +105,9 @@ func main() {
 // 所以就没有必要返回他们的指针了。make函数的函数签名如下：
 
 // func make(t Type, size ...IntegerType) Type
+
+// make函数是无可替代的，我们在使用slice、map以及channel的时候，都需要使用make进行初始化，
+// 然后才可以对它们进行操作。这个我们在上一章中都有说明，关于channel我们会在后续的章节详细说明。
+
+// 本节开始的示例中var b map[string]int只是声明变量b是一个map类型的变量，
+// 需要像下面的示例代码一样使用make函数进行初始化操作之后，才能对其进行键值对赋值：
