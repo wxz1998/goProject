e2bcf73b479c9f29305694dfd87132a5047cc211
wxz1998 <wxz18737676272@163.com>
2020-11-28 11:57:35 +0800

    20201128

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println()
 }
+
+// 自定义类型
+// 在Go语言中有一些基本的数据类型，如string、整型、浮点型、布尔等数据类型， Go语言中可以使用type关键字来定义自定义类型。
+
+// 自定义类型是定义了一个全新的类型。我们可以基于内置的基本类型定义，也可以通过struct定义。例如：
+
+//将MyInt定义为int类型
+// type MyInt int
+
+// 通过type关键字的定义，MyInt就是一种新的类型，它具有int的特性。
