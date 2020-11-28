c6ec274b9cb7add85eac71ddaa1be6ef5db25821
wxz1998 <wxz18737676272@163.com>
2020-11-28 11:58:10 +0800

    20201128

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -15,3 +15,14 @@ func main() {
 // type MyInt int
 
 // 通过type关键字的定义，MyInt就是一种新的类型，它具有int的特性。
+
+// 类型别名
+// 类型别名是Go1.9版本添加的新功能。
+
+// 类型别名规定：TypeAlias只是Type的别名，本质上TypeAlias与Type是同一个类型。就像一个孩子小时候有小名、乳名，上学后用学名，英语老师又会给他起英文名，但这些名字都指的是他本人。
+
+// type TypeAlias = Type
+// 我们之前见过的rune和byte就是类型别名，他们的定义如下：
+
+// type byte = uint8
+// type rune = int32
