319560bf56c42452285a3ee7e11b51b87951022f
wxz1998 <wxz18737676272@163.com>
2020-11-30 16:34:14 +0800

    20201130

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -125,10 +125,10 @@ package main
 
 // 取结构体的地址实例化
 // 使用&对结构体进行取地址操作相当于对该结构体类型进行了一次new实例化操作。
-p3 := &person{}
-fmt.Printf("%T\n", p3)     //*main.person
-fmt.Printf("p3=%#v\n", p3) //p3=&main.person{name:"", city:"", age:0}
-p3.name = "七米"
-p3.age = 30
-p3.city = "成都"
-fmt.Printf("p3=%#v\n", p3) //p3=&main.person{name:"七米", city:"成都", age:30}
+// p3 := &person{}
+// fmt.Printf("%T\n", p3)     //*main.person
+// fmt.Printf("p3=%#v\n", p3) //p3=&main.person{name:"", city:"", age:0}
+// p3.name = "七米"
+// p3.age = 30
+// p3.city = "成都"
+// fmt.Printf("p3=%#v\n", p3) //p3=&main.person{name:"七米", city:"成都", age:30}
