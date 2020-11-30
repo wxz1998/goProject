9346f466209d661361b0c5dc7dbc5c51f3e29eb5
wxz1998 <wxz18737676272@163.com>
2020-11-30 17:19:42 +0800

    20201130

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // 自定义类型
 // 在Go语言中有一些基本的数据类型，如string、整型、浮点型、布尔等数据类型， Go语言中可以使用type关键字来定义自定义类型。
 
@@ -470,34 +468,34 @@ import "fmt"
 // Go语言中使用结构体也可以实现其他编程语言中面向对象的继承。
 
 //Animal 动物
-type Animal struct {
-	name string
-}
+// type Animal struct {
+// 	name string
+// }
 
-func (a *Animal) move() {
-	fmt.Printf("%s会动！\n", a.name)
-}
+// func (a *Animal) move() {
+// 	fmt.Printf("%s会动！\n", a.name)
+// }
 
-//Dog 狗
-type Dog struct {
-	Feet    int8
-	*Animal //通过嵌套匿名结构体实现继承
-}
+// //Dog 狗
+// type Dog struct {
+// 	Feet    int8
+// 	*Animal //通过嵌套匿名结构体实现继承
+// }
 
-func (d *Dog) wang() {
-	fmt.Printf("%s会汪汪汪~\n", d.name)
-}
+// func (d *Dog) wang() {
+// 	fmt.Printf("%s会汪汪汪~\n", d.name)
+// }
 
-func main() {
-	d1 := &Dog{
-		Feet: 4,
-		Animal: &Animal{ //注意嵌套的是结构体指针
-			name: "乐乐",
-		},
-	}
-	d1.wang() //乐乐会汪汪汪~
-	d1.move() //乐乐会动！
-}
+// func main() {
+// 	d1 := &Dog{
+// 		Feet: 4,
+// 		Animal: &Animal{ //注意嵌套的是结构体指针
+// 			name: "乐乐",
+// 		},
+// 	}
+// 	d1.wang() //乐乐会汪汪汪~
+// 	d1.move() //乐乐会动！
+// }
 
 // 结构体字段的可见性
 // 结构体中字段大写开头表示可公开访问，小写表示私有（仅在定义当前结构体的包中可访问）。
