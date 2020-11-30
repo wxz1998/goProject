a5ed7a5e170ba271797bb23c009e86cfc04e9a42
wxz1998 <wxz18737676272@163.com>
2020-11-30 16:38:35 +0800

    20201130

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -214,3 +214,27 @@ package main
 
 // var v struct{}
 // fmt.Println(unsafe.Sizeof(v))  // 0
+
+// 面试题
+// 请问下面代码的执行结果是什么？
+
+// type student struct {
+// 	name string
+// 	age  int
+// }
+
+// func main() {
+// 	m := make(map[string]*student)
+// 	stus := []student{
+// 		{name: "小王子", age: 18},
+// 		{name: "娜扎", age: 23},
+// 		{name: "大王八", age: 9000},
+// 	}
+
+// 	for _, stu := range stus {
+// 		m[stu.name] = &stu
+// 	}
+// 	for k, v := range m {
+// 		fmt.Println(k, "=>", v.name)
+// 	}
+// }
