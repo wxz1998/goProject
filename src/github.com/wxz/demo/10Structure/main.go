0c2bc6525a8d6911b16a9c28c2561a4e615bc080
wxz1998 <wxz18737676272@163.com>
2020-11-30 17:33:33 +0800

    20201130

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // 自定义类型
 // 在Go语言中有一些基本的数据类型，如string、整型、浮点型、布尔等数据类型， Go语言中可以使用type关键字来定义自定义类型。
 
@@ -584,32 +582,32 @@ import "fmt"
 // 结构体和方法补充知识点
 // 因为slice和map这两种数据类型都包含了指向底层数据的指针，因此我们在需要复制它们时要特别注意。我们来看下面的例子：
 
-type Person struct {
-	name   string
-	age    int8
-	dreams []string
-}
+// type Person struct {
+// 	name   string
+// 	age    int8
+// 	dreams []string
+// }
 
 // func (p *Person) SetDreams(dreams []string) {
 // 	p.dreams = dreams
 // }
 
-func main() {
-	p1 := Person{name: "小王子", age: 18}
-	data := []string{"吃饭", "睡觉", "打豆豆"}
-	p1.SetDreams(data)
+// func main() {
+// 	p1 := Person{name: "小王子", age: 18}
+// 	data := []string{"吃饭", "睡觉", "打豆豆"}
+// 	p1.SetDreams(data)
 
-	// 你真的想要修改 p1.dreams 吗？
-	data[1] = "不睡觉"
-	fmt.Println(p1.dreams) // ?
-}
+// 	// 你真的想要修改 p1.dreams 吗？
+// 	data[1] = "不睡觉"
+// 	fmt.Println(p1.dreams) // ?
+// }
 
 // 正确的做法是在方法中使用传入的slice的拷贝进行结构体赋值。
 
-func (p *Person) SetDreams(dreams []string) {
-	p.dreams = make([]string, len(dreams))
-	copy(p.dreams, dreams)
-}
+// func (p *Person) SetDreams(dreams []string) {
+// 	p.dreams = make([]string, len(dreams))
+// 	copy(p.dreams, dreams)
+// }
 
 // 同样的问题也存在于返回值slice和map的情况，在实际编码过程中一定要注意这个问题。
 
