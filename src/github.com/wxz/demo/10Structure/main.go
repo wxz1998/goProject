3efc02b0a89dd04c9a76942762ba504b4d2e8999
wxz1998 <wxz18737676272@163.com>
2020-11-30 16:44:01 +0800

    20201130

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -335,3 +335,22 @@ package main
 // 需要修改接收者中的值
 // 接收者是拷贝代价比较大的大对象
 // 保证一致性，如果有某个方法使用了指针接收者，那么其他的方法也应该使用指针接收者。
+
+// 任意类型添加方法
+// 在Go语言中，接收者的类型可以是任何类型，不仅仅是结构体，任何类型都可以拥有方法。
+// 举个例子，我们基于内置的int类型使用type关键字可以定义新的自定义类型，然后为我们的自定义类型添加方法。
+
+// //MyInt 将int定义为自定义MyInt类型
+// type MyInt int
+
+// //SayHello 为MyInt添加一个SayHello的方法
+// func (m MyInt) SayHello() {
+// 	fmt.Println("Hello, 我是一个int。")
+// }
+// func main() {
+// 	var m1 MyInt
+// 	m1.SayHello() //Hello, 我是一个int。
+// 	m1 = 100
+// 	fmt.Printf("%#v  %T\n", m1, m1) //100  main.MyInt
+// }
+// 注意事项： 非本地类型不能定义方法，也就是说我们不能给别的包的类型定义方法。
