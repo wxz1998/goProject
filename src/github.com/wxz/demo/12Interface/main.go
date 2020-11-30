4ad0b50e50631b8b8eec8f40c2f4718adc43a5e0
wxz1998 <wxz18737676272@163.com>
2020-11-30 18:21:33 +0800

    20201130

--- src/github.com/wxz/demo/12Interface/main.go
+++ src/github.com/wxz/demo/12Interface/main.go
@@ -53,3 +53,13 @@ type dog struct{}
 func (d dog) move() {
 	fmt.Println("狗会动")
 }
+
+// 此时实现接口的是dog类型：
+func main() {
+	var x Mover
+	var wangcai = dog{} // 旺财是dog类型
+	x = wangcai         // x可以接收dog类型
+	var fugui = &dog{}  // 富贵是*dog类型
+	x = fugui           // x可以接收*dog类型
+	x.move()
+}
