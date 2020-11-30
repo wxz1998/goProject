caab224447cdde2f78a66f55e8084504feacd8f0
wxz1998 <wxz18737676272@163.com>
2020-11-30 17:25:43 +0800

    20201130

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 // 自定义类型
 // 在Go语言中有一些基本的数据类型，如string、整型、浮点型、布尔等数据类型， Go语言中可以使用type关键字来定义自定义类型。
 
@@ -509,49 +504,49 @@ import (
 // JSON(JavaScript Object Notation) 是一种轻量级的数据交换格式。易于人阅读和编写。同时也易于机器解析和生成。
 // JSON键值对是用来保存JS对象的一种方式，键/值对组合中的键名写在前面并用双引号""包裹，使用冒号:分隔，然后紧接着值；多个键值之间使用英文,分隔。
 
-//Student 学生
-type Student struct {
-	ID     int
-	Gender string
-	Name   string
-}
+// //Student 学生
+// type Student struct {
+// 	ID     int
+// 	Gender string
+// 	Name   string
+// }
 
-//Class 班级
-type Class struct {
-	Title    string
-	Students []*Student
-}
+// //Class 班级
+// type Class struct {
+// 	Title    string
+// 	Students []*Student
+// }
 
-func main() {
-	c := &Class{
-		Title:    "101",
-		Students: make([]*Student, 0, 200),
-	}
-	for i := 0; i < 10; i++ {
-		stu := &Student{
-			Name:   fmt.Sprintf("stu%02d", i),
-			Gender: "男",
-			ID:     i,
-		}
-		c.Students = append(c.Students, stu)
-	}
-	//JSON序列化：结构体-->JSON格式的字符串
-	data, err := json.Marshal(c)
-	if err != nil {
-		fmt.Println("json marshal failed")
-		return
-	}
-	fmt.Printf("json:%s\n", data)
-	//JSON反序列化：JSON格式的字符串-->结构体
-	str := `{"Title":"101","Students":[{"ID":0,"Gender":"男","Name":"stu00"},{"ID":1,"Gender":"男","Name":"stu01"},{"ID":2,"Gender":"男","Name":"stu02"},{"ID":3,"Gender":"男","Name":"stu03"},{"ID":4,"Gender":"男","Name":"stu04"},{"ID":5,"Gender":"男","Name":"stu05"},{"ID":6,"Gender":"男","Name":"stu06"},{"ID":7,"Gender":"男","Name":"stu07"},{"ID":8,"Gender":"男","Name":"stu08"},{"ID":9,"Gender":"男","Name":"stu09"}]}`
-	c1 := &Class{}
-	err = json.Unmarshal([]byte(str), c1)
-	if err != nil {
-		fmt.Println("json unmarshal failed!")
-		return
-	}
-	fmt.Printf("%#v\n", c1)
-}
+// func main() {
+// 	c := &Class{
+// 		Title:    "101",
+// 		Students: make([]*Student, 0, 200),
+// 	}
+// 	for i := 0; i < 10; i++ {
+// 		stu := &Student{
+// 			Name:   fmt.Sprintf("stu%02d", i),
+// 			Gender: "男",
+// 			ID:     i,
+// 		}
+// 		c.Students = append(c.Students, stu)
+// 	}
+// 	//JSON序列化：结构体-->JSON格式的字符串
+// 	data, err := json.Marshal(c)
+// 	if err != nil {
+// 		fmt.Println("json marshal failed")
+// 		return
+// 	}
+// 	fmt.Printf("json:%s\n", data)
+// 	//JSON反序列化：JSON格式的字符串-->结构体
+// 	str := `{"Title":"101","Students":[{"ID":0,"Gender":"男","Name":"stu00"},{"ID":1,"Gender":"男","Name":"stu01"},{"ID":2,"Gender":"男","Name":"stu02"},{"ID":3,"Gender":"男","Name":"stu03"},{"ID":4,"Gender":"男","Name":"stu04"},{"ID":5,"Gender":"男","Name":"stu05"},{"ID":6,"Gender":"男","Name":"stu06"},{"ID":7,"Gender":"男","Name":"stu07"},{"ID":8,"Gender":"男","Name":"stu08"},{"ID":9,"Gender":"男","Name":"stu09"}]}`
+// 	c1 := &Class{}
+// 	err = json.Unmarshal([]byte(str), c1)
+// 	if err != nil {
+// 		fmt.Println("json unmarshal failed!")
+// 		return
+// 	}
+// 	fmt.Printf("%#v\n", c1)
+// }
 
 // 结构体标签（Tag）
 // Tag是结构体的元信息，可以在运行的时候通过反射的机制读取出来。 Tag在结构体字段的后方定义，由一对反引号包裹起来，具体的格式如下：
