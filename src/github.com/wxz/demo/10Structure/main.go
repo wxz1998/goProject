1408aeb55673a519ad333f42be356f3037ea1f0c
wxz1998 <wxz18737676272@163.com>
2020-11-30 16:39:47 +0800

    20201130

--- src/github.com/wxz/demo/10Structure/main.go
+++ src/github.com/wxz/demo/10Structure/main.go
@@ -254,3 +254,42 @@ package main
 
 // p9 := newPerson("张三", "沙河", 90)
 // fmt.Printf("%#v\n", p9) //&main.person{name:"张三", city:"沙河", age:90}
+
+// 方法和接收者
+// Go语言中的方法（Method）是一种作用于特定类型变量的函数。这种特定类型变量叫做接收者（Receiver）。接收者的概念就类似于其他语言中的this或者 self。
+
+// 方法的定义格式如下：
+
+// func (接收者变量 接收者类型) 方法名(参数列表) (返回参数) {
+//     函数体
+// }
+// 其中，
+
+// 接收者变量：接收者中的参数变量名在命名时，官方建议使用接收者类型名称首字母的小写，而不是self、this之类的命名。例如，Person类型的接收者变量应该命名为 p，Connector类型的接收者变量应该命名为c等。
+// 接收者类型：接收者类型和参数类似，可以是指针类型和非指针类型。
+// 方法名、参数列表、返回参数：具体格式与函数定义相同。
+// 举个例子：
+
+// //Person 结构体
+// type Person struct {
+// 	name string
+// 	age  int8
+// }
+
+// //NewPerson 构造函数
+// func NewPerson(name string, age int8) *Person {
+// 	return &Person{
+// 		name: name,
+// 		age:  age,
+// 	}
+// }
+
+// //Dream Person做梦的方法
+// func (p Person) Dream() {
+// 	fmt.Printf("%s的梦想是学好Go语言！\n", p.name)
+// }
+
+// func main() {
+// 	p1 := NewPerson("小王子", 25)
+// 	p1.Dream()
+// }
