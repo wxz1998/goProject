85e66cda6536aee544ac35f9fab74eff21c20d72
wxz1998 <wxz18737676272@163.com>
2020-12-02 20:17:21 +0800

    20201202

--- src/github.com/wxz/demo/13Reflect/main.go
+++ src/github.com/wxz/demo/13Reflect/main.go
@@ -1,18 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
 /*
  * @Author: zut.wxz
  * @Date: 2020-12-02 15:12:58
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-02 18:00:52
+ * @LastEditTime: 2020-12-02 20:17:06
  * @Description:
  */
 
+/*
+// TypeOf
+// 在Go语言中，使用reflect.TypeOf()函数可以获得任意值的类型对象（reflect.Type），程序通过类型对象可以访问任意值的类型信息。
+func reflectType(x interface{}) {
+	v := reflect.TypeOf(x)
+	fmt.Printf("type:%v\n", v)
+}
+func main() {
+	var a float32 = 3.14
+	reflectType(a) // type:float32
+	var b int64 = 100
+	reflectType(b) // type:int64
+}
+
+// type name和type kind
+// 在反射中关于类型还划分为两种：类型（Type）和种类（Kind）。
+// 因为在Go语言中我们可以使用type关键字构造很多自定义类型，而种类（Kind）就是指底层的类型，
+//但在反射中，当需要区分指针、结构体等大品种的类型时，就会用到种类（Kind）。
+// 举个例子，我们定义了两个指针类型和两个结构体类型，通过反射查看它们的类型和种类。
 type myInt int64
 
 func reflectType(x interface{}) {
@@ -40,4 +54,62 @@ func main() {
 	var e = book{title: "《跟小王子学Go语言》"}
 	reflectType(d) // type:person kind:struct
 	reflectType(e) // type:book kind:struct
+}
+
+// 通过反射获取值
+func reflectValue(x interface{}) {
+	v := reflect.ValueOf(x)
+	k := v.Kind()
+	switch k {
+	case reflect.Int64:
+		// v.Int()从反射中获取整型的原始值，然后通过int64()强制类型转换
+		fmt.Printf("type is int64, value is %d\n", int64(v.Int()))
+	case reflect.Float32:
+		// v.Float()从反射中获取浮点型的原始值，然后通过float32()强制类型转换
+		fmt.Printf("type is float32, value is %f\n", float32(v.Float()))
+	case reflect.Float64:
+		// v.Float()从反射中获取浮点型的原始值，然后通过float64()强制类型转换
+		fmt.Printf("type is float64, value is %f\n", float64(v.Float()))
+	}
+}
+func main() {
+	var a float32 = 3.14
+	var b int64 = 100
+	reflectValue(a) // type is float32, value is 3.140000
+	reflectValue(b) // type is int64, value is 100
+	// 将int类型的原始值转换为reflect.Value类型
+	c := reflect.ValueOf(10)
+	fmt.Printf("type c :%T\n", c) // type c :reflect.Value
+}
+
+// IsNil()常被用于判断指针是否为空；IsValid()常被用于判定返回值是否有效。
+func main() {
+	// *int类型空指针
+	var a *int
+	fmt.Println("var a *int IsNil:", reflect.ValueOf(a).IsNil())
+	// nil值
+	fmt.Println("nil IsValid:", reflect.ValueOf(nil).IsValid())
+	// 实例化一个匿名结构体
+	b := struct{}{}
+	// 尝试从结构体中查找"abc"字段
+	fmt.Println("不存在的结构体成员:", reflect.ValueOf(b).FieldByName("abc").IsValid())
+	// 尝试从结构体中查找"abc"方法
+	fmt.Println("不存在的结构体方法:", reflect.ValueOf(b).MethodByName("abc").IsValid())
+	// map
+	c := map[string]int{}
+	// 尝试从map中查找一个不存在的键
+	fmt.Println("map中不存在的键：", reflect.ValueOf(c).MapIndex(reflect.ValueOf("娜扎")).IsValid())
+}
+*/
+
+type StructField struct {
+	// Name是字段的名字。PkgPath是非导出字段的包路径，对导出字段该字段为""。
+	// 参见http://golang.org/ref/spec#Uniqueness_of_identifiers
+	Name      string
+	PkgPath   string
+	Type      Type      // 字段的类型
+	Tag       StructTag // 字段的标签
+	Offset    uintptr   // 字段在结构体中的字节偏移量
+	Index     []int     // 用于Type.FieldByIndex时的索引切片
+	Anonymous bool      // 是否匿名字段
 }
