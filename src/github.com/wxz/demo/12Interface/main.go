4af0c27fb920a03eea9f95c0027c46d5e71355fe
wxz1998 <wxz18737676272@163.com>
2020-12-02 15:11:31 +0800

    20201202

--- src/github.com/wxz/demo/12Interface/main.go
+++ src/github.com/wxz/demo/12Interface/main.go
@@ -6,7 +6,7 @@ import "fmt"
  * @Author: zut.wxz
  * @Date: 2020-11-30 18:11:19
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-02 15:10:12
+ * @LastEditTime: 2020-12-02 15:11:22
  * @Description:
  */
 
@@ -256,3 +256,22 @@ func main() {
 	x = b
 	fmt.Printf("type:%T value:%v\n", x, x)
 }
+
+// 空接口的应用
+// 空接口作为函数的参数
+// 使用空接口实现可以接收任意类型的函数参数。
+
+// 空接口作为函数参数
+// func show(a interface{}) {
+// 	fmt.Printf("type:%T value:%v\n", a, a)
+// }
+
+// 空接口作为map的值
+// 使用空接口实现可以保存任意值的字典。
+
+// 空接口作为map值
+// var studentInfo = make(map[string]interface{})
+// studentInfo["name"] = "沙河娜扎"
+// studentInfo["age"] = 18
+// studentInfo["married"] = false
+// fmt.Println(studentInfo)
