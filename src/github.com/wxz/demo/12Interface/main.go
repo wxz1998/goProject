011379c5c101c6d98f84b06b57ab197c89a73fc5
wxz1998 <wxz18737676272@163.com>
2020-12-02 12:24:49 +0800

    20201202

--- src/github.com/wxz/demo/12Interface/main.go
+++ src/github.com/wxz/demo/12Interface/main.go
@@ -4,7 +4,7 @@ package main
  * @Author: zut.wxz
  * @Date: 2020-11-30 18:11:19
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-02 12:23:48
+ * @LastEditTime: 2020-12-02 12:24:38
  * @Description:
  */
 
@@ -125,12 +125,6 @@ type Sayer interface {
 }
 
 // Mover 接口
-/**
- * @functionname:
- * @description:
- * @param {*}
- * @return {*}
- */
 type Mover interface {
 	move()
 }
