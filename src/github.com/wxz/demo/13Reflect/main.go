dd2cd7a6a79fd819bf85560c91d7de63e9f9f30d
wxz1998 <wxz18737676272@163.com>
2020-12-02 16:07:04 +0800

    20201202

--- src/github.com/wxz/demo/13Reflect/main.go
+++ src/github.com/wxz/demo/13Reflect/main.go
@@ -1,15 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 /*
  * @Author: zut.wxz
  * @Date: 2020-12-02 15:12:58
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-02 15:24:28
+ * @LastEditTime: 2020-12-02 16:06:56
  * @Description:
  */
 
-import "fmt"
-
+// 在Go语言中，使用reflect.TypeOf()函数可以获得任意值的类型对象（reflect.Type），程序通过类型对象可以访问任意值的类型信息。
+func reflectType(x interface{}) {
+	v := reflect.TypeOf(x)
+	fmt.Printf("type:%v\n", v)
+}
 func main() {
-	fmt.Println()
+	var a float32 = 3.14
+	reflectType(a) // type:float32
+	var b int64 = 100
+	reflectType(b) // type:int64
 }
