fafd9878cfc2de17ef71ca7cf1ca29a2898409b7
wxz1998 <wxz18737676272@163.com>
2020-12-02 15:24:34 +0800

    20201202

--- src/github.com/wxz/demo/13Reflect/main.go
+++ src/github.com/wxz/demo/13Reflect/main.go
@@ -1,11 +1,12 @@
+package main
+
 /*
  * @Author: zut.wxz
  * @Date: 2020-12-02 15:12:58
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-02 15:22:21
+ * @LastEditTime: 2020-12-02 15:24:28
  * @Description:
  */
-package main
 
 import "fmt"
 
