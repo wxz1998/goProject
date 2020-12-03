4d38d4985c93e959571042e034359223d480688c
wxz1998 <wxz18737676272@163.com>
2020-12-03 21:37:36 +0800

    20201203

--- src/github.com/wxz/demo/14Goroutine/main.go
+++ src/github.com/wxz/demo/14Goroutine/main.go
@@ -6,7 +6,7 @@ import "fmt"
  * @Author: zut.wxz
  * @Date: 2020-12-03 21:08:03
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-03 21:34:08
+ * @LastEditTime: 2020-12-03 21:37:27
  * @Description:
  */
 
@@ -32,12 +32,14 @@ func main() {
 */
 
 func recv(c chan int) {
-	ret := <-c
+	ret := <-c // 接收 并发
 	fmt.Println("接收成功", ret)
 }
 func main() {
 	ch := make(chan int)
 	go recv(ch) // 启用goroutine从通道接收值
-	ch <- 10
+	ch <- 10    // 发送到管道
 	fmt.Println("发送成功")
 }
+
+// 使用无缓冲通道进行通信将导致发送和接收的goroutine同步化。因此，无缓冲通道也被称为同步通道
