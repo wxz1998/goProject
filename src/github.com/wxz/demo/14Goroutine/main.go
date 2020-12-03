c8ac9bc2f3339f0459a0e8af9442110d44de7483
wxz1998 <wxz18737676272@163.com>
2020-12-03 21:42:53 +0800

    20201203

--- src/github.com/wxz/demo/14Goroutine/main.go
+++ src/github.com/wxz/demo/14Goroutine/main.go
@@ -6,7 +6,7 @@ import "fmt"
  * @Author: zut.wxz
  * @Date: 2020-12-03 21:08:03
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-03 21:38:18
+ * @LastEditTime: 2020-12-03 21:42:45
  * @Description:
  */
 
@@ -44,8 +44,39 @@ func main() {
 
 // 使用无缓冲通道进行通信将导致发送和接收的goroutine同步化。因此，无缓冲通道也被称为同步通道
 
-func main() {
+/* func main() {
 	ch := make(chan int, 1) // 创建一个容量为1的有缓冲区通道
 	ch <- 10
 	fmt.Println("发送成功")
+	exit := <-ch
+	fmt.Println("接受", exit)
+	close(ch)
+} */
+
+// channel 练习
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	// 开启goroutine将0~100的数发送到ch1中
+	go func() {
+		for i := 0; i < 100; i++ {
+			ch1 <- i
+		}
+		close(ch1)
+	}()
+	// 开启goroutine从ch1中接收值，并将该值的平方发送到ch2中
+	go func() {
+		for {
+			i, ok := <-ch1 // 通道关闭后再取值ok=false
+			if !ok {
+				break
+			}
+			ch2 <- i * i
+		}
+		close(ch2)
+	}()
+	// 在主goroutine中从ch2中接收值打印
+	for i := range ch2 { // 通道关闭后会退出for range循环
+		fmt.Println(i)
+	}
 }
