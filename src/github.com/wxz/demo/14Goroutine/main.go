e55f26281bc10d8b6f3a0da2bb04d180717d2259
wxz1998 <wxz18737676272@163.com>
2020-12-03 21:21:19 +0800

    20201203

--- src/github.com/wxz/demo/14Goroutine/main.go
+++ src/github.com/wxz/demo/14Goroutine/main.go
@@ -4,17 +4,26 @@ package main
  * @Author: zut.wxz
  * @Date: 2020-12-03 21:08:03
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-03 21:19:14
+ * @LastEditTime: 2020-12-03 21:21:10
  * @Description:
  */
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func hello() {
-	fmt.Println("hello goroutine")
+var wg sync.WaitGroup
+
+func hello(i int) {
+	defer wg.Done() // goroutine结束就登记-1
+	fmt.Println("Hello Goroutine!", i)
 }
-
 func main() {
-	go hello()
-	fmt.Println("main goroutine done")
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1) // 启动一个goroutine就登记+1
+		go hello(i)
+	}
+	wg.Wait() // 等待所有登记的goroutine都结束
 }
