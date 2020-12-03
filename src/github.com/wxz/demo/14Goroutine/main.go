f837ab772881375c27a7d86f7c77548930433041
wxz1998 <wxz18737676272@163.com>
2020-12-03 22:36:07 +0800

    20201203

--- src/github.com/wxz/demo/14Goroutine/main.go
+++ src/github.com/wxz/demo/14Goroutine/main.go
@@ -1,14 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 /*
  * @Author: zut.wxz
  * @Date: 2020-12-03 21:08:03
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-03 21:46:09
+ * @LastEditTime: 2020-12-03 22:35:17
  * @Description:
  */
 
@@ -53,8 +51,7 @@ func main() {
 	exit := <-ch
 	fmt.Println("接受", exit)
 	close(ch)
-} */
-
+} */ /*
 // channel 练习
 func main() {
 	// runtime.GOMAXPROCS(2)
@@ -83,4 +80,65 @@ func main() {
 		fmt.Println(i)
 	}
 	// time.Sleep(time.Second)
+}
+*/
+
+/*
+// channel 练习
+func main() {
+	// runtime.GOMAXPROCS(2)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	// 开启goroutine将0~100的数发送到ch1中
+	go func() {
+		for i := 0; i < 100; i++ {
+			ch1 <- i
+		}
+		close(ch1)
+	}()
+	// 开启goroutine从ch1中接收值，并将该值的平方发送到ch2中
+	go func() {
+		for {
+			i, ok := <-ch1 // 通道关闭后再取值ok=false
+			if !ok {
+				break
+			}
+			ch2 <- i * i
+		}
+		close(ch2)
+	}()
+	// 在主goroutine中从ch2中接收值打印
+	for i := range ch2 { // 通道关闭后会退出for range循环
+		fmt.Println(i)
+	}
+	// time.Sleep(time.Second)
+}
+*/
+
+// 单向通道
+func counter(out chan<- int) {
+	for i := 0; i < 100; i++ {
+		out <- i
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for i := range in {
+		out <- i * i
+	}
+	close(out)
+}
+func printer(in <-chan int) {
+	for i := range in {
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go counter(ch1)
+	go squarer(ch2, ch1)
+	printer(ch2)
 }
