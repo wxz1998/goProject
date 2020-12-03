3e6820b344ef0ae1c9c544cf4b7657a719706139
wxz1998 <wxz18737676272@163.com>
2020-12-03 21:34:27 +0800

    20201203

--- src/github.com/wxz/demo/14Goroutine/main.go
+++ src/github.com/wxz/demo/14Goroutine/main.go
@@ -1,29 +1,43 @@
 package main
 
+import "fmt"
+
 /*
  * @Author: zut.wxz
  * @Date: 2020-12-03 21:08:03
  * @LastEditors: zut.wxz
- * @LastEditTime: 2020-12-03 21:21:10
+ * @LastEditTime: 2020-12-03 21:34:08
  * @Description:
  */
 
-import (
-	"fmt"
-	"sync"
-)
+/*
+func a() {
+	for i := 1; i < 10; i++ {
+		fmt.Println("A:", i)
+	}
+}
 
-var wg sync.WaitGroup
+func b() {
+	for i := 1; i < 10; i++ {
+		fmt.Println("B:", i)
+	}
+}
 
-func hello(i int) {
-	defer wg.Done() // goroutine结束就登记-1
-	fmt.Println("Hello Goroutine!", i)
+func main() {
+	runtime.GOMAXPROCS(2)
+	go a()
+	go b()
+	time.Sleep(time.Second)
+}
+*/
+
+func recv(c chan int) {
+	ret := <-c
+	fmt.Println("接收成功", ret)
 }
 func main() {
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1) // 启动一个goroutine就登记+1
-		go hello(i)
-	}
-	wg.Wait() // 等待所有登记的goroutine都结束
+	ch := make(chan int)
+	go recv(ch) // 启用goroutine从通道接收值
+	ch <- 10
+	fmt.Println("发送成功")
 }
