b6970e6c1625786055d152dbc0b3a0a624c2c6dd
wxz1998 <wxz18737676272@163.com>
2020-12-03 21:16:26 +0800

    20201203

---
+++ src/github.com/wxz/demo/14Goroutine/main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+ * @Author: zut.wxz
+ * @Date: 2020-12-03 21:08:03
+ * @LastEditors: zut.wxz
+ * @LastEditTime: 2020-12-03 21:16:15
+ * @Description:
+ */
+
+import "fmt"
+
+func main() {
+	fmt.Println()
+}
