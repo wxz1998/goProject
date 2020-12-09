7c274089e0c19f08cca8f44277dd5a7c5f9b7cbe
wxz1998 <wxz18737676272@163.com>
2020-12-09 16:08:51 +0800

    20201209

--- src/wxz.com/demo1/01 Variable declarations/main.go
+++ src/wxz.com/demo1/01 Variable declarations/main.go
@@ -5,7 +5,7 @@ import "fmt"
 // go语言函数外的语句必须以关键字开头
 var name = "wxz"
 
-// 常量声明
+// 常量声明 函数体外声明的变量为全局变量
 const pi = 3.1415
 
 // 多个初始化
@@ -13,7 +13,7 @@ var name1, age1 = "bbb", 22
 
 // 变量的声明方式
 func main() {
-	// 赋值操作只能在函数体内
+	// 局部变量
 	age := 18
 	fmt.Println(name, age, pi)
 	fmt.Println(name1, age1)
